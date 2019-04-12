a27820231dcef62ec31d5f907b30f459dde3918a
François Parmentier <francois.parmentier@gmail.com>
2019-04-12 14:48:09 +0200

    :zap: Make entrypoint serve data directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,13 @@
 
 cd /app
 
-echo "Installing packages..."
-/app/installPackages
-
 echo "Loading environnment variables..."
 /app/config2vars > /app/config.env
 chmod 755 /app/config.env
 source /app/config.env
 
-#echo "Starting cron..."
-#/app/crontab &
-
-sleep 2
+echo "Downloading data..."
+/app/downloadData
 
 echo "Starting server..."
-exec npx serve $*
+exec npx serve data
